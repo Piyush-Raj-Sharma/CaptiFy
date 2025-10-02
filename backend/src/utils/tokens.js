c964c7dfc440533dbc8dd97a5456e27cf369ba68
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-03 00:56:26 +0530

    added a util function to generate Access & Refresh Tokens

--- backend/src/utils/tokens.js
+++ backend/src/utils/tokens.js
@@ -1,7 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateTokens = (user) => {
-    const accessToken = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: "15m",
-    });
-}
+  const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign(
+    { id: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  return { accessToken, refreshToken };
+};
