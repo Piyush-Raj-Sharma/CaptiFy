db0616347667d9e932d1e3f2f45569ae9b136867
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-04 17:30:37 +0530

    added post routes for creating posts

---
+++ backend/src/routes/post.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const verifyAccessToken = require('../middlewares/auth.middleware');
+
+const router = express.Router();
+
+router.post('/', verifyAccessToken)
+
+
+module.exports = router;
