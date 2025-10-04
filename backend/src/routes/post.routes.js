270650b279a5c65febac818909df75b93266f22b
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-04 18:17:08 +0530

    added & configured multer

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const verifyAccessToken = require('../middlewares/auth.middleware');
+const verifyAccessToken = require("../middlewares/auth.middleware");
 // const createPostController = require('../controller/createPost.controller')
+const multer = require("multer");
 
+const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
-router.post('/', verifyAccessToken, createPostController);
-
+router.post(
+  "/",
+  verifyAccessToken,
+  upload.single("image"),
+  createPostController
+);
 
 module.exports = router;
