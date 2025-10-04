85b94df1de61ebc23cc0aecf7a1cc3932c010719
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-04 17:28:05 +0530

    Updated Auth Middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,12 +1,16 @@
 const jwt = require("jsonwebtoken");
+const userModel = require('../models/user.model');
 
-exports.verifyAccessToken = (req, res, next) => {
+exports.verifyAccessToken = async (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await userModel.findOne({
+      _id: decoded.id
+    })
+    req.user = user;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
